30003b8c4420f8ebade2658ecf3b18ba4cb00eb8
cpprhtn <cpprhtn@naver.com>
2024-04-13 23:18:03 +0900

    Update t15.py

--- t15.py
+++ t15.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
 import json
 
 chromedriver_path = './chromedriver'
@@ -19,7 +18,7 @@ capabilities = DesiredCapabilities.CHROME.copy()
 capabilities['goog:loggingPrefs'] = {'browser': 'ALL'}
 
 driver = webdriver.Chrome(service= Service(chromedriver_path), options=options)
-url = 'https://google.com'
+url = 'https://naver.com'
 
 driver.get(url)
 wait = WebDriverWait(driver, 20)
@@ -37,16 +36,16 @@ jsonData = []
 for entry in network_requests:
     jsonData.append({
         "Name": entry["name"],
-        "Status": entry["initiatorType"],
-        "Type": entry["entryType"],
+        "Status": entry["responseStatus"],
+        "Type": entry["initiatorType"],
         "Size": entry["transferSize"],
         "Time": entry["duration"]
     })
 
-print(json.dumps(jsonData))
+# print(json.dumps(jsonData))
 
-with open('network.json', 'w') as json_file:
-    json.dump(network_requests, json_file)
+# with open('network2.json', 'w') as json_file:
+#     json.dump(network_requests, json_file)
 
 with open('network_requests.json', 'w') as json_file2:
     json.dump(jsonData, json_file2, indent=4)
